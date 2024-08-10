74ac8cee6e4a71487c1e31d3f88b9ee3640b679c
cachenow <36331626+cachenow@users.noreply.github.com>
2024-08-10 08:42:21 +0000

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 #rm -rf feeds/packages/net/mosdns
