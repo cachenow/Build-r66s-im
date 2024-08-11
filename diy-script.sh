e760af693886f8ba941e7c933fe013edc9ec0131
cachenow <36331626+cachenow@users.noreply.github.com>
2024-08-11 19:27:49 +0000

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -40,6 +40,9 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 git_sparse_clone master https://github.com/syb999/openwrt-23.05.3 package/network/services/msd_lite
 git_sparse_clone openwrt-23.05 https://github.com/openwrt/packages net/tailscale
 
+# 添加rtl88x2bu驱动
+git clone --depth=1 https://github.com/mirobiala/rtl88x2bu-cl package/kernel/rtl88x2bu-cl
+
 # Themes
 git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
