9a9bfb98280d74e25ee899678239b363392ed9d3
cachenow <36331626+cachenow@users.noreply.github.com>
2024-08-11 16:20:59 +0000

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -32,7 +32,7 @@ function git_sparse_clone() {
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
